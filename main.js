6e60dc8a021ac28bcd2a3d445e26fc4e3e32afae
profa-janaina <111704392+profa-janaina@users.noreply.github.com>
2023-09-01 17:52:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+
+song = "";
+objects = [];
+status = "";
+
+function preload()
+{
+	// 1- Escreva um código para carregar o arquivo de áudio
+}
+
+function setup() {
+  // 2-  Crie o canvas, configure e o acesse a webcam
+}
+
+function start() {
+ // 3-  Carregue o modelo cocossd no código
+  document.getElementById("status").innerHTML = "Status: Detectando Objetos";
+}
+
+function modelLoaded() {
+  console.log("Modelo Carregado!")
+  status = true;
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+
+function draw() {
+  image(video, 0, 0, 380, 380);
+      if(status != "")
+      {
+        r =  random(255);
+        g =  random(255);
+        b =  random(255);      
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status: Objetos Detectados";
+ 
+          fill(r,g,b);
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke(r,g,b);
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+         
+          if(objects[i].label == "person")
+          {
+            document.getElementById("numberOfObjects").innerHTML = "Bebê encontrado";
+            console.log("stop");
+            song.stop();
+            
+          }
+          else
+          {
+            document.getElementById("numberOfObjects").innerHTML = "Bebê não encontrado";
+            console.log("play"); 
+            song.play();
+           
+          }
+         }
+
+        if(objects.length == 0)
+        {
+          document.getElementById("numberOfObjects").innerHTML = "Bebê não encontrado";
+          console.log("play"); 
+          song.play();
+          
+        }
+      }
+}
